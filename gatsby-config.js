4920718300de640c8f4e2cd53d4caa7751936d39
Patryk Pozniak <patryk.pozniak@reachplc.com>
2020-12-26 22:56:19 +0000

    Set up typescript and eslint. Update npmrc settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-typescript",
     {
       resolve: "gatsby-source-filesystem",
       options: {
