d0b0b2bcbfc1d61263ab3f87db5600b8c6187146
Patryk Pozniak <patryk.pozniak@reachplc.com>
2020-12-28 19:59:59 +0000

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/favicon.png",
       },
     },
     {
